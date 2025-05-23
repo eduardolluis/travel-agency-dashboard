1937cfc98d3ad2ac30c62980dd14ea34abc2d0e7
Eduardo De La Cruz <eduardodelacruzg4@gmail.com>
2025-05-23 07:59:40 -0400

    Routing

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,19 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig } from '@react-router/dev/routes';
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+  {
+    path: 'admin',
+    file: './routes/admin/adminLayout.tsx',
+
+    children: [
+      {
+        path: 'dashboard',
+        file: './routes/admin/dashboard.tsx',
+      },
+      {
+        path: 'allUsers',
+        file: './routes/admin/allUsers.tsx',
+      },
+    ],
+  },
+] satisfies RouteConfig;
---
+++ app/routes/admin/allUsers.tsx
@@ -0,0 +1,6 @@
+
+const allUsers = () => {
+  return <div>Users Table</div>;
+};
+
+export default allUsers;
